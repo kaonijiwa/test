542e17009aaf1c28a44502f79bdea6dc878f2612
hello <pzghost123@gmail.com>
2021-03-23 13:36:18 +0800

    单链表

---
+++ suanfa/algorithm/test/src/dataStructure/linkedList/SingleLinkedList.java
@@ -0,0 +1,176 @@
+package dataStructure.linkedList;
+
+public class SingleLinkedList {
+    public static void main(String[] args) {
+        HeroNode node01 = new HeroNode(4, "宋江", "及时雨");
+        HeroNode node02 = new HeroNode(1, "卢俊义", "玉麒麟");
+        HeroNode node03 = new HeroNode(3, "吴用", "智多星");
+        HeroNode node04 = new HeroNode(2, "吴用", "智多星");
+        HeroNode node05 = new HeroNode(2, "吕布", "奉先");
+        HeroNode node06 = new HeroNode(1, "张飞", "翼德");
+        HeroNode node07 = new HeroNode(5, "张飞", "翼德");
+        LinkedListDemo demo = new LinkedListDemo();
+        demo.addByOrder(node01);
+        demo.addByOrder(node02);
+        demo.addByOrder(node03);
+        demo.addByOrder(node04);
+        demo.addByOrder(node05);
+        System.out.println(demo.head);
+        demo.update(node06);
+        demo.update(node07);
+        System.out.println(demo.head);
+        demo.delete(node01);
+        System.out.println(demo.head);
+    }
+}
+
+class LinkedListDemo {
+
+    //头节点，头节点不能动,头节点不存放数据
+    public HeroNode head = new HeroNode(0, "", "");
+
+    //添加节点到单向链表
+    public void add(HeroNode heroNode) {
+        //因为head节点不能动，因此我们需要一个辅助遍历temp
+        HeroNode temp = head;
+        //当退出while循环时，temp就指向了链表最后的节点
+        while (true) {
+            if (temp.next == null) {
+                break;
+            }
+            temp = temp.next;
+        }
+        temp.next = heroNode;
+    }
+
+    //有序的添加
+    public void addByOrder(HeroNode heroNode) {
+        HeroNode temp = head;
+        boolean flag = false;
+        while (true) {
+            if (temp.next == null) {
+                break;
+            }
+            //temp的下一个节点的no大于要插入节点的no
+            if (temp.next.no > heroNode.no) {
+                break;
+            } else if (temp.next.no == heroNode.no) {
+                //temp的下一个节点的no等于要插入节点的no
+                flag = true;
+                break;
+            }
+            temp = temp.next;
+        }
+        //不能添加，编号已经存在
+        if (flag) {
+            System.out.println("此英雄的编号已经存在了：" + heroNode);
+            return;
+        }
+        heroNode.next = temp.next;
+        temp.next = heroNode;
+    }
+
+    //修改节点的信息，根据no修改
+    public void update(HeroNode heroNode) {
+        //判断是否为空
+        if (head.next == null) {
+            System.out.println("链表为空");
+            return;
+        }
+        HeroNode temp = head;
+        //表示是否找到了该节点
+        boolean flag = false;
+        while (true) {
+            if (temp == null) {
+                //链表已经遍历完毕
+                break;
+            }
+            if (temp.no == heroNode.no) {
+                System.out.println("找到了");
+                flag = true;
+                break;
+            }
+            temp = temp.next;
+        }
+        //根据flag判断是否找到要修改的节点
+        if (flag) {
+            temp.name = heroNode.name;
+            temp.nickName = heroNode.nickName;
+            return;
+        }
+        System.out.println("没有找到这个编号的节点，不能修改：" + heroNode);
+    }
+
+    //删除节点
+    public void delete(HeroNode heroNode) {
+        //判断是否为空
+        if (head.next == null) {
+            System.out.println("链表为空");
+            return;
+        }
+        HeroNode temp = head;
+        //表示是否找到了该节点
+        boolean flag = false;
+        while (true) {
+            if (temp.next == null) {
+                //链表已经遍历完毕
+                break;
+            }
+            if (temp.next.no == heroNode.no) {
+                System.out.println("找到了");
+                flag = true;
+                break;
+            }
+            temp = temp.next;
+        }
+        //根据flag判断是否找到要修改的节点
+        if (flag) {
+            temp.next = temp.next.next;
+            return;
+        }
+        System.out.println("没有找到这个编号的节点，不能删除：" + heroNode);
+    }
+
+
+    //显示链表
+    public void show() {
+        //如果链表为空
+        if (head.next == null) {
+            return;
+        }
+        HeroNode temp = head.next;
+        while (true) {
+            if (temp == null) {
+                break;
+            }
+            System.out.println(temp);
+            temp = temp.next;
+        }
+    }
+
+}
+
+//定义节点
+class HeroNode {
+    public int no;
+    public String name;
+    public String nickName;
+    //指向下一个节点
+    public HeroNode next;
+
+    public HeroNode(int no, String name, String nickName) {
+        this.no = no;
+        this.name = name;
+        this.nickName = nickName;
+    }
+
+    @Override
+    public String toString() {
+        return "HeroNode{" +
+                "no=" + no +
+                ", name='" + name + '\'' +
+                ", nickName='" + nickName + '\'' +
+                ", next=" + next +
+                '}';
+    }
+}
